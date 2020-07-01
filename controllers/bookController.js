0edd537bf3c9bb91c27387989d7be6dc66f23f25
wbusaka <wilbusaka@gmail.com>
2020-07-01 19:21:19 +0300

    add the relevant controllers for the book model and routes

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,12 +1,43 @@
+'use strict'
 let Book = require('../models/book')
+let Author = require('../models/author')
+let Genre = require('../models/genre')
+let BookInstance = require('../models/bookinstance')
+let async = require('async')
 
-exports.index = (req, res)=> {
-    res.send('NOT IMPLEMENTED: Site Home Page')
+exports.index = (req, res)=>{
+
+    async.parallel({
+        book_count(callback){
+            Book.countDocuments({}, callback)
+        },
+        book_instance_count(callback){
+            BookInstance.countDocuments({}, callback)
+        },
+        book_instance_available_count(callback){
+            BookInstance.countDocuments({status: 'Available'}, callback)
+        },
+        author_count(callback){
+            Author.countDocuments({}, callback)
+        },
+        genre_count(callback){
+            Genre.countDocuments({}, callback)
+        }
+    }, (err, results)=>{
+        res.render('index', {title: 'Local library Home', error: err, data: results})
+    })
 }
 
 // Display list of all books.
-exports.book_list = (req, res)=> {
-    res.send('NOT IMPLEMENTED: Book list')
+exports.book_list = (req, res, next)=> {
+
+    Book.find({}, 'title author')
+    .populate('author')
+    .exec((err, list_books)=>{
+        if (err) return next(err)
+        //Successful, so render
+        res.render('book_list', {title: 'Book List', book_list: list_books})
+    })
 }
 
 // Display detail page for a specific book.
