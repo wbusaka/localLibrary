cdae0e0c28dba90bccc598be56f0821722969a38
wbusaka <wilbusaka@gmail.com>
2020-07-01 17:05:15 +0300

    modify the controllers to render data from the database

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,15 @@ let Genre = require('../models/genre')
 
 // Display list of all Genre.
 exports.genre_list = (req, res)=> {
-    res.send('NOT IMPLEMENTED: Genre list')
+    
+    Genre.find()
+    .populate('author')
+    .sort([['name', 'ascending']])
+    .exec(function (err, list_genres) {
+      if (err) { return next(err); }
+      //Successful, so render
+      res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
+    })
 }
 
 // Display detail page for a specific Genre.
